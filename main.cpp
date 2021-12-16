5f58c9056fd7e58237cfae47d9bed6beb9e832b1
Egor <egor.lit007@mail.ru>
2021-12-16 20:19:55 +0300

    Complex class.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,10 @@
 #include <iostream>
 
+#include "complex.hpp"
 using namespace std;
-class A
-{
-public:
-    A() {cout << "I was constructed!" << endl;}
-    A(int a) {cout << "I was constructed with one param!" << endl;}
-    ~A() {cout << "I was destructed" << endl;}
-    static void print() {cout << "PRINT" << endl;}
-};
 
-int main() {
-    A (b);
-    A::print();
-    cout << "SHIS" << endl;
+int main()
+{
     return 0;
 }
+
